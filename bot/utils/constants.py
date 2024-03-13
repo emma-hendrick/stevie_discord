a3d7b084ec1b19429bae90cfefe8c86a07c6741a
Emma Hendrick <mhendrick000@gmail.com>
2024-03-12 17:03:39 -0700

    updates and fixes

--- bot/utils/constants.py
+++ bot/utils/constants.py
@@ -1,6 +1,12 @@
 # OS allows us to retrieve our evironment variables
 import os
+from os.path import join, dirname
+from dotenv import load_dotenv
 
+# Load variables from .env
+dotenv_path = join(dirname(__file__), "../../.env")
+print(dotenv_path)
+load_dotenv(dotenv_path)
 
 # All the program constants
 class CONST:
